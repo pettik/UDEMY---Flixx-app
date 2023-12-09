475cab598a16d89409cb230f2a2673c8bf92346e
pettik <petrbednarski@gmail.com>
2023-12-09 17:10:09 +0100

    search results pagination

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const global = {
     type: '',
     page: 1,
     totalPages: 1,
+    totalResults: 0,
   },
   api: {
     apiKey: '5c6bdcd939359fd514270f993c5e79b9',
@@ -15,7 +16,7 @@ const global = {
 // Display 20 most popular MOVIES
 async function displayPopularMovies() {
   const { results } = await fetchAPIData('movie/popular'); // {results} <= {resutls:{...},{...},{...}}
-  results.forEach((movie) => {
+  results.forEach(movie => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -41,7 +42,7 @@ async function displayPopularMovies() {
 // Display 20 most popular TV shows
 async function displayPopularShows() {
   const { results } = await fetchAPIData('tv/popular'); // {results} <= {resutls:{...},{...},{...}}
-  results.forEach((show) => {
+  results.forEach(show => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -95,7 +96,7 @@ async function displayMovieDetails() {
     </p>
     <h5>Genres</h5>
     <ul class="list-group">
-      ${movie.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      ${movie.genres.map(genre => `<li>${genre.name}</li>`).join('')}
     </ul>
     <a href="${
       movie.homepage
@@ -118,7 +119,7 @@ async function displayMovieDetails() {
   </ul>
   <h4>Production Companies</h4>
   <div class="list-group">${movie.production_companies
-    .map((company) => `<span>${company.name}</span>`)
+    .map(company => `<span>${company.name}</span>`)
     .join(', ')}</div>
 </div>
   `;
@@ -155,7 +156,7 @@ async function displayShowDetails() {
     </p>
     <h5>Genres</h5>
     <ul class="list-group">
-      ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      ${show.genres.map(genre => `<li>${genre.name}</li>`).join('')}
     </ul>
     <a href="${
       show.homepage
@@ -175,7 +176,7 @@ async function displayShowDetails() {
   </ul>
   <h4>Production Companies</h4>
   <div class="list-group">${show.production_companies
-    .map((company) => `<span>${company.name}</span>`)
+    .map(company => `<span>${company.name}</span>`)
     .join(', ')}</div>
 </div>
   `;
@@ -216,7 +217,11 @@ async function search() {
 
   if (global.search.term !== '' && global.search.term !== null) {
     // @todo - make request and display results
-    const { results, total_pages, page } = await searchAPIData();
+    const { results, total_pages, page, total_results } = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
 
     if (results.length === 0) {
       showAlert('No results found');
@@ -231,7 +236,12 @@ async function search() {
 }
 
 function displaySearchResults(results) {
-  results.forEach((result) => {
+  //clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
+  results.forEach(result => {
     const div = document.createElement('div');
     div.classList.add('card');
     div.innerHTML = `
@@ -262,14 +272,57 @@ function displaySearchResults(results) {
     </div>
     `;
 
+    document.querySelector('#search-results-heading').innerHTML = `
+    <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+
+    `;
     document.querySelector('#search-results').appendChild(div);
+  });
+
+  displayPagination();
+}
+
+// Create and Display Pagination For Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
   });
 }
 
 // Display Slider Movies
 async function displaySlider() {
   const { results } = await fetchAPIData('movie/now_playing');
-  results.forEach((movie) => {
+  results.forEach(movie => {
     const div = document.createElement('div');
     div.classList.add('swiper-slide');
 
@@ -327,15 +380,15 @@ async function fetchAPIData(endpoint) {
   hideSpinner();
   return data;
 }
-// Make Request to Search
 
+// Make Request to SearchAPIData
 async function searchAPIData() {
   const API_KEY = global.api.apiKey; //shouldn't be here in production;
   const API_URL = global.api.apiUrl;
 
   showSpinner();
   const response = await fetch(
-    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`
+    `${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`
   ); //should be on SERVER if it is in production
 
   const data = await response.json();
@@ -354,7 +407,7 @@ function hideSpinner() {
 function hightlightActiveLink() {
   const links = document.querySelectorAll('.nav-link');
 
-  links.forEach((link) => {
+  links.forEach(link => {
     if (link.getAttribute('href') === global.currentPage) {
       link.classList.add('active');
     }
